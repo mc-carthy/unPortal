376377db6daa76a1b63e45055e3950188865d09e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-01-19 22:16:54 +0000

    Create render texture at runtime and sharpen image

---
+++ Assets/PortalTextureSetup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour 
+{
+    public Camera camB;
+    public Material camMatB;
+
+    private void Awake()
+    {
+        if (camB.targetTexture != null)
+        {
+            camB.targetTexture.Release();
+        }
+        camB.targetTexture = new RenderTexture(Screen.width, Screen.width, 24);
+        camMatB.mainTexture = camB.targetTexture;
+    }
+}
---
+++ Assets/scripts/PortalCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PortalCamera : MonoBehaviour 
+{
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    private void Update() 
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDiffBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDiffBetweenPortalRotations, Vector3.up);
+        Vector3 newCamDir = portalRotationalDifference * playerCamera.forward;
+
+        transform.rotation = Quaternion.LookRotation(newCamDir, Vector3.up);
+    }
+    
+}
